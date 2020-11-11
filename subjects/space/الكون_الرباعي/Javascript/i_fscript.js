fa463578c1d87e410354b0f680ab31c7381cbadf
Hussein Alaa <huseinalzubaidi@hotmail.com>
2020-11-11 12:23:38 +0200

    ok

--- subjects/space/الكون_الرباعي/Javascript/i_fscript.js
+++ subjects/space/الكون_الرباعي/Javascript/i_fscript.js
@@ -3,13 +3,14 @@ titleIn = "الكون الرباعي";
 adPhoto = "https://quran-m.com/firas/ar_photo/1/22.jpg"
 adDiscription = "الكون الرباعي و هو مفهوم بسيط لأبعاد الكون , الابعاد المكانية الثلاثة الطول و العرض و الارتفاع و البعد الزمني الزمان , وهو الكون الذي نعيش فيه , و لكن ماذا لو كان الكون اكثر من اربع أبعاد ؟<br>"
 namePpath = "https://www.trouko.co/subjects/space/الكون_الرباعي/"
-infSuGuPage = "تاريخ النشر 2020/9/1 "+"التصنيفات / الفضاء"
+infSuGuPageOn = '<p> التصنيفات / الفضاء </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> تاريخ النشر : 2020 /9 / 2 </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">query_builder</i>'
 nameP = namePpath.replace('https://www.trouko.co/',"")
 addTitleIpage = document.getElementById(nameP+'_pag_n_232');
 addt(titleIn)
 function addt(m){
 var textDis = adDiscription.slice(0, 80);
 var ap = document.createElement('div')
-ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<p class="infoPageSuQu">'+infSuGuPage+'</p>'+"<p class='p_t_inf'>"+textDis+"...</p>" + '<a class="link_t_read_p" href="'+namePpath+'">'+" قراءة المزيد "+"</a> </div>"
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+"<p class='p_t_inf'>"+textDis+"...</p>" + '<a class="link_t_read_p" href="'+namePpath+'">'+" قراءة المزيد "+"</a> </div>"
 addTitleIpage.append(ap)
 }
