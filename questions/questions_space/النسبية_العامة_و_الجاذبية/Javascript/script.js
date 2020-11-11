bcc1e5d7a2219f744fdd83343680c2a28fa1dacc
Hussein Alaa <huseinalzubaidi@hotmail.com>
2020-11-11 16:56:14 +0200

    FF

--- questions/questions_space/النسبية_العامة_و_الجاذبية/Javascript/script.js
+++ questions/questions_space/النسبية_العامة_و_الجاذبية/Javascript/script.js
@@ -14,7 +14,7 @@ var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_
     addSourcesAndQuotesfive_na = "";
     addSourcesAndQuotesSix_na = "";
     addSourcesAndQuotesSeven_na
-    addSugg_vrOne  = "https://www.trouko.co/questions/questions_space/ما_هي_الطاقة_المضلمه/";
+    addSugg_vrOne  = "https://www.trouko.co/questions/questions_space/ما_هي_الطاقة_المضلمة/";
     addSugg_vrTwo  = "https://www.trouko.co/questions/questions_space/ما_هو_عمر_الكون/";
     addSugg_vrThree  = "https://www.trouko.co/subjects/space/مجرة_درب_التبانة/";
     addSugg_vrFour  = "";
