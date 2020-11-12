3fe13303bde2ba9e4018e7f21ecec0f105dfa1a4
Hussein Alaa <huseinalzubaidi@hotmail.com>
2020-11-12 18:30:49 +0200

    F

--- container_s_q/script.js
+++ container_s_q/script.js
@@ -1,21 +1,8 @@
-(function(w){
-    w.addEventListener('load', function(){
-        const   btn_left = document.getElementById('btn-left'),
-                btn_right = document.getElementById('btn-right'),
-                content = document.getElementById('sub_r_s');
-        const content_scroll_width = content.scrollWidth;
-        let content_scoll_left = content.scrollLeft;
-        btn_right.addEventListener('click', () => {
-            content_scoll_left += 300;
-            if (content_scoll_left >= content_scroll_width) { content_scoll_left = content_scroll_width; }
-            content.scrollLeft = content_scoll_left;
-        });
-        btn_left.addEventListener('click', () => {
-            content_scoll_left -= 300;
-            if (content_scoll_left <= 0) {
-                content_scoll_left = 0;
-            }
-            content.scrollLeft = content_scoll_left;
-        });
-    });
-})(window);
+var xReq = new XMLHttpRequest();
+xReq.open('GET','../data.js');
+
+function randoPag(){
+    var xData = JSON.parse(xReq.responseText);
+    var r = Math.floor(Math.random() * xData.length) 
+    location.href = xData[r].linkPage
+}
