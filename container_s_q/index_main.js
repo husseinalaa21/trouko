811ee6808106cccf3b3be9487f10620426b0ed5a
Hussein Alaa <huseinalzubaidi@hotmail.com>
2020-12-20 12:24:16 +0200

    FU

--- container_s_q/index_main.js
+++ container_s_q/index_main.js
@@ -196,20 +196,20 @@ var change = 'open';
 })
 
 window.onscroll = function() {
-    checS();
     myFunction()
 };
-
-function checS() {
-  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 66) {
-      document.getElementById("menu_main").style.position = "fixed";
-     // document.getElementById("header").style.boxShadow = "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.089)";
-
-  } else {
-      document.getElementById("menu_main").style.position = "static";
-      //document.getElementById("header").style.boxShadow = "";
-  }
-}
+var tolsShor = `<div class="isVis isVisbl" id="isVisb" onclick="isVisb();"><p id="wtvis">أضهار</p> <i class="fa fa-eye" style="font-size: 15px;margin-top: 3px"></i></div><div style="display: none;" id="myBar"></div>`
+var tols = `<div class="control_contact_p" ><div class="isVis" id="isVisb" onclick="isVisb();"><p id="wtvis">أخفاء</p> <i class='fas fa-low-vision' style="font-size: 15px;margin-top: 3px"></i></div>
+<div id="control_contact_p"><div class="control_c_b">
+<div class="c_c_b_black" onclick="changeCo('bl')"> C </div>
+<div class="c_c_b_white" onclick="changeCo('wh')"> C </div>
+<div class="c_c_b_yellow" onclick="changeCo('yell')"> C </div>
+<div class="c_c_b_def" onclick="changeCo('def')"> C </div>
+</div><div class="control_text_ss">
+<div class="t_t_small">   Aa </div>
+<div class="t_t_med">  Aa </div>
+<div class="t_t_larg">  Aa </div>
+</div><div class="header"><div class="progress-container"><div class="progress-bar" id="myBar"></div></div></div></div></div>`;
 
 function myFunction() {
     var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
@@ -217,3 +217,118 @@ function myFunction() {
     var scrolled = (winScroll / height) * 110;
     document.getElementById("myBar").style.width = scrolled + "%";
   }
+  
+var isV = false;
+  function isVisb(){
+      if(isV == false){
+          isV = true;
+          document.getElementById("contr_Pa").innerHTML = tolsShor
+      } else {
+        document.getElementById("wtvis").innerHTML = " أخفاء "
+        document.getElementById("contr_Pa").innerHTML = tols
+        isV = false;
+      }
+  }
+
+var bl,wh,yell,def;
+var c = ["one","two","three","four","five","six","seven"]
+
+function chngBT(backG,backG2,colo,border,backHeader,backSug){
+    document.body.style.backgroundColor = backG
+    document.body.style.backgroundColor = backG
+    for(var i=0; i< 6; i++){
+        document.getElementsByClassName("ad_sub_"+c[i])[0].style.backgroundColor = backG
+        document.getElementsByClassName("ad_sub_"+c[i])[0].style.color = colo
+    }
+    document.getElementsByClassName("ch_sub_")[0].style.backgroundColor = backG
+    document.getElementsByClassName("ch_sub_")[1].style.backgroundColor = backG
+    document.getElementsByClassName("index__page__main")[0].style.backgroundColor = backG
+    document.getElementsByClassName("main_un_t_inf_o")[0].style.backgroundColor = backG
+    document.getElementsByClassName("header_m_t")[0].style.backgroundColor = backG
+    document.getElementsByClassName("s_tas")[0].style.backgroundColor = backG
+    document.getElementsByClassName("main_menu_main")[0].style.backgroundColor = backG
+    document.getElementsByClassName("index_p_sugg")[0].style.backgroundColor = backG
+    document.getElementsByClassName("index_page")[0].style.backgroundColor = backG
+    document.getElementsByClassName("index_page")[1].style.backgroundColor = backG
+    document.querySelectorAll(".index_list_i a")[0].style.backgroundColor = backG
+    document.getElementsByClassName("title_sour_an_qou")[0].style.backgroundColor = backG
+    document.getElementsByClassName("sou_an_quo_content")[0].style.backgroundColor = backG
+    document.getElementsByClassName("notesQ")[0].style.backgroundColor = backG
+
+    document.getElementsByClassName("pText_Image")[0].style.backgroundColor = backG2
+    
+    document.getElementsByClassName("ch_sub_")[0].style.color = colo
+    document.getElementsByClassName("Content__for__index__m")[0].style.color = colo
+    document.getElementsByClassName("main_un_t_inf_o")[0].style.color = colo
+    document.getElementsByClassName("index_page")[0].style.color = colo
+    document.getElementsByClassName("title_sour_an_qou")[0].style.color = colo
+    document.getElementsByClassName("notesQ")[0].style.color = colo
+    var qiuLine = document.getElementsByClassName("sources_")
+    for(var qq = 0; qq < qiuLine.length; qq++){
+        document.getElementsByClassName("sources_")[qq].style.color = colo
+    }
+    var specEL = document.querySelectorAll(".index_list_ a")
+    for(var ii = 0; ii < specEL.length; ii++){
+        specEL[ii].style.color = colo
+    }
+    var specELP = document.querySelectorAll(".index_sugg_p p a")
+    for(var iii = 0; iii < specELP.length; iii++){
+        specELP[iii].style.color = colo
+    }
+    document.querySelectorAll(".index_list_i a")[0].style.color = colo
+    document.querySelectorAll(".index_list_i a")[1].style.color = colo
+    document.getElementsByClassName("ch_sub_")[1].style.color = colo
+    document.getElementsByClassName("tit_index__")[0].style.color = colo
+    document.querySelectorAll(".adConIn a")[0].style.color = colo
+    document.getElementsByClassName("index_p_sugg")[0].style.color = colo
+    
+    document.querySelectorAll(".tit_index__ div")[0].style.borderBottom = "1.5px solid "+border
+    document.querySelectorAll(".main_un_t_inf_o")[0].style.borderBottom = "0"
+    document.getElementsByClassName("s_tas")[0].style.border = backG
+    document.querySelectorAll(".index_list_i a")[0].style.border = "0"
+    document.getElementsByClassName("index_p_sugg")[0].style.border = "0"
+    document.getElementsByClassName("main_menu_main")[0].style.borderBottom = "1.5px solid "+border
+    document.getElementsByClassName("text_sub_s")[0].style.borderBottom = "1.5px solid "+border
+    document.getElementsByClassName("text_sub_s")[1].style.borderBottom = "1.5px solid "+border
+    document.getElementsByClassName("menu_main")[0].style.backgroundColor = backHeader
+
+    // ch two
+    var sugBavk = document.getElementsByClassName("p_t_inf")
+    for(var ik = 0; ik < sugBavk.length; ik++){
+        document.getElementsByClassName("p_t_inf")[ik].style.backgroundColor = backSug
+        document.querySelectorAll(".p_t_inf p")[ik].style.color = colo
+
+        document.getElementsByClassName("infoPageSuQu")[ik].style.backgroundColor = backSug
+        document.getElementsByClassName("infoPageSuQu")[ik].style.borderBottom = backSug
+        document.getElementsByClassName("infoPageSuQuTw")[ik].style.backgroundColor = backSug
+        document.getElementsByClassName("infoPageSuQuTw")[ik].style.border = backSug
+
+        document.getElementsByClassName("s_sub_t")[ik].style.backgroundColor = backG2
+        document.getElementsByClassName("s_sub_t")[ik].style.color = colo
+    }
+
+    var sugBavk2 = document.getElementsByClassName("sugg_p_t_bo_23Tw")
+    for(var ij = 0; ij < sugBavk2.length; ij++){
+        document.getElementsByClassName("s_sub_tTw")[ij].style.backgroundColor = backG2
+        document.getElementsByClassName("s_sub_tTw")[ij].style.color = colo
+        document.getElementsByClassName("infoPageSuQuTw")[ij+ik].style.backgroundColor = backSug
+        document.getElementsByClassName("infoPageSuQuTw")[ij+ik].style.border = backSug
+        document.querySelectorAll(".p_t_infTw p")[ij].style.color = colo
+        document.getElementsByClassName("p_t_infTw")[ij].style.backgroundColor = backSug
+        document.getElementsByClassName("infoPageSuQuTwTw")[ij].style.backgroundColor = backSug
+    }
+    document.getElementsByClassName("no_result")[0].style.colo = colo
+}
+function changeCo(m){
+    if(m == "bl"){
+        chngBT("#1b262c","#666666a2","#dbd8e3","#525252","","#1b262c")
+    } else if(m== "wh"){
+        chngBT("#f1f1f1"," #f1f1f14b","#0f0f0f","#f1f1f1","#f1f1f1","#f1f1f1")
+    } else if(m == "yell"){
+        chngBT("#f8f7de","#f8f7de60","#0f0f0f","#e8e8e8","#f8f7de","#f8f7de")
+    } else if(m== "def"){
+        chngBT("","","","#d3d2d2","","")
+    } else {
+        return false
+    }
+}
