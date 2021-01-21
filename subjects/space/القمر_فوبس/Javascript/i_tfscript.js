68a3bd77f07a532480e7ebb7a498a4c890b27c41
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-01-21 11:33:39 +0200

    T

---
+++ subjects/space/القمر_فوبس/Javascript/i_tfscript.js
@@ -0,0 +1,17 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "القمر فوبس";
+adPhoto = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQbLgpK-aONBZTtjFc9pGoVQlg67MSPi9Or6-Ivxjiw7VVHD6n&s"
+adDiscription = "تشير إحدى النماذج الجديدة التي وضعها العلماء إلى أن الأخاديد الموجودة على سطح قمر المريخ فوبوس ناجمة عن القوى المدية، أي قوة الجذب المتبادلة بين كل من المريخ وقمره .<br /><br />و يضن علماء الفلك أن الأخاديد الطويلة والعميقة المنتشرة طوليا على سطح قمر المريخ فوبوس Phobos، ما هي إلا إشارات مبكرة على الانهيار المحتمل الذي ستتعرض له بنية القمر ما سيؤدي في نهاية المطاف إلى تحطمه بشكل كامل. "
+namePpath = "https://www.trouko.co/subjects/space/القمر_فوبس/"
+infSuGuPageOn = '<p>  المواضيع / الفضاء </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الاثنين  , 18 يناير 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://www.trouko.co/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_x_242');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.className = "suggTwo"
+ap.innerHTML ='<div class="img_p_sugg_pTw"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_iTw" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23Tw'> <a class='s_sub_tTw' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQuTw">'+infSuGuPageOn+'</div>'+"<div class='p_t_infTw'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_pTw" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTwTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
