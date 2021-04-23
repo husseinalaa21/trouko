b35dc95d77c24c40ee14013508cc65d28e1e1123
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-23 05:47:20 +0300

    F

--- data.js
+++ data.js
@@ -1,13 +1,64 @@
 [
 {
+ "linkPage" : "https://www.trouko.com/subjects/else/أبل/", 
+ "namePath" : "أبل" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/أجمل_دول_العالم/", 
+ "namePath" : "أجمل دول العالم" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/تيسلا_موتورز/", 
+ "namePath" : "تيسلا موتورز" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/لا_تنخدع_بالربح_من_الانترنت/", 
+ "namePath" : "لا تنخدع بالربح من الانترنت" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/مقارنة_بين_السيارات_التي_تعمل_بالكهرباء_و_التي_تعمل_بالبانزين/", 
+ "namePath" : "مقارنة بين السيارات التي تعمل بالكهرباء و التي تعمل بالبانزين" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/هل_الفيس_بوك_مربح_حقاً/", 
+ "namePath" : "هل الفيس بوك مربح حقاً" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/أقمار_كوكب_المريخ/", 
+ "namePath" : "أقمار كوكب المريخ" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/استعمار_المريخ/", 
+ "namePath" : "استعمار المريخ" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/الأشعة_الكونية/", 
+ "namePath" : "الأشعة الكونية" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/السفر_بين_النجوم/", 
+ "namePath" : "السفر بين النجوم" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/القمر_فوبس/", 
+ "namePath" : "القمر فوبس" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/الكون_الرباعي/", 
+ "namePath" : "الكون الرباعي" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/تضاريس_كوكب_المريخ/", 
+ "namePath" : "تضاريس كوكب المريخ" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/سبيس_اكس/", 
+ "namePath" : "سبيس اكس" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/مجرة_درب_التبانة/", 
+ "namePath" : "مجرة درب التبانة" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/ناسا/", 
+ "namePath" : "ناسا" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/space/ناسا_تصنع_المستحيل_و_تحول_ثاني_اكسيد_الكاربون_الى_اوكسجين_نقي/", 
+ "namePath" : "ناسا تصنع المستحيل و تحول ثاني اكسيد الكاربون الى اوكسجين نقي" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/technology/اهم_ثلاث_مواقع_لتعليم_البرمجة/", 
+ "namePath" : "اهم ثلاث مواقع لتعليم البرمجة" 
+},{
  "linkPage" : "https://www.trouko.com/questions/questions_else/البورصه/", 
  "namePath" : "البورصه" 
 },{
  "linkPage" : "https://www.trouko.com/questions/questions_else/ماذا_حدث_لأسهم_شركة_Game_Stop/", 
  "namePath" : "ماذا حدث لأسهم شركة Game Stop" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_physics/كيف_يعمل_المايكرويف/", 
- "namePath" : "كيف يعمل المايكرويف" 
 },{
  "linkPage" : "https://www.trouko.com/questions/questions_space/أقرب_كوكب_صالح_للحياة_من_الأرض/", 
  "namePath" : "أقرب كوكب صالح للحياة من الأرض" 
@@ -45,61 +96,13 @@
  "linkPage" : "https://www.trouko.com/questions/questions_technology/ما_هي_شاشات_الحبرالالكتروني/", 
  "namePath" : "ما هي شاشات الحبرالالكتروني" 
 },{
- "linkPage" : "https://www.trouko.com/subjects/else/أبل/", 
- "namePath" : "أبل" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/أجمل_دول_العالم/", 
- "namePath" : "أجمل دول العالم" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/تيسلا_موتورز/", 
- "namePath" : "تيسلا موتورز" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/لا_تنخدع_بالربح_من_الانترنت/", 
- "namePath" : "لا تنخدع بالربح من الانترنت" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/مقارنة_بين_السيارات_التي_تعمل_بالكهرباء_و_التي_تعمل_بالبانزين/", 
- "namePath" : "مقارنة بين السيارات التي تعمل بالكهرباء و التي تعمل بالبانزين" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/هل_الفيس_بوك_مربح_حقاً/", 
- "namePath" : "هل الفيس بوك مربح حقاً" 
+ "linkPage" : "https://www.trouko.com/questions/questions_physics/كيف_يعمل_المايكرويف/", 
+ "namePath" : "كيف يعمل المايكرويف" 
 },{
  "linkPage" : "https://www.trouko.com/subjects/physics/الطاقة_المتجددة/", 
  "namePath" : "الطاقة المتجددة" 
 },{
  "linkPage" : "https://www.trouko.com/subjects/physics/عالم_الكم_و_الزمان/", 
  "namePath" : "عالم الكم و الزمان" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/أقمار_كوكب_المريخ/", 
- "namePath" : "أقمار كوكب المريخ" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/استعمار_المريخ/", 
- "namePath" : "استعمار المريخ" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/الأشعة_الكونية/", 
- "namePath" : "الأشعة الكونية" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/السفر_بين_النجوم/", 
- "namePath" : "السفر بين النجوم" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/القمر_فوبس/", 
- "namePath" : "القمر فوبس" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/الكون_الرباعي/", 
- "namePath" : "الكون الرباعي" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/تضاريس_كوكب_المريخ/", 
- "namePath" : "تضاريس كوكب المريخ" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/سبيس_اكس/", 
- "namePath" : "سبيس اكس" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/مجرة_درب_التبانة/", 
- "namePath" : "مجرة درب التبانة" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/ناسا/", 
- "namePath" : "ناسا" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/technology/اهم_ثلاث_مواقع_لتعليم_البرمجة/", 
- "namePath" : "اهم ثلاث مواقع لتعليم البرمجة" 
 }
 ]
