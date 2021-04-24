a3d6febd04ca258ecaf051cd9f0d03ae4676bae6
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-24 21:54:53 +0300

    F

--- data.js
+++ data.js
@@ -1,46 +1,16 @@
 [
 {
- "linkPage" : "https://www.trouko.com/subjects/else/أبل/", 
- "namePath" : "أبل" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/أجمل_دول_العالم/", 
- "namePath" : "أجمل دول العالم" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/تيسلا_موتورز/", 
- "namePath" : "تيسلا موتورز" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/لا_تنخدع_بالربح_من_الانترنت/", 
- "namePath" : "لا تنخدع بالربح من الانترنت" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/ما_هي_اسعار_المنازل_في_كندا/", 
- "namePath" : "ما هي اسعار المنازل في كندا" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/مشروع_أبل_لصناعة_السيارات/", 
- "namePath" : "مشروع أبل لصناعة السيارات" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/مقارنة_بين_السيارات_التي_تعمل_بالكهرباء_و_التي_تعمل_بالبانزين/", 
- "namePath" : "مقارنة بين السيارات التي تعمل بالكهرباء و التي تعمل بالبانزين" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/else/هل_الفيس_بوك_مربح_حقاً/", 
- "namePath" : "هل الفيس بوك مربح حقاً" 
-},{
  "linkPage" : "https://www.trouko.com/subjects/geography/جزيرة_الغد_و_جزيرة_الأمس/", 
  "namePath" : "جزيرة الغد و جزيرة الأمس" 
 },{
  "linkPage" : "https://www.trouko.com/subjects/geography/حقائق_حول_القارة_الثامنة_المكتشفة_حديثاً/", 
  "namePath" : "حقائق حول القارة الثامنة المكتشفة حديثاً" 
 },{
- "linkPage" : "https://www.trouko.com/questions/questions_else/البورصه/", 
- "namePath" : "البورصه" 
+ "linkPage" : "https://www.trouko.com/subjects/physics/الطاقة_المتجددة/", 
+ "namePath" : "الطاقة المتجددة" 
 },{
- "linkPage" : "https://www.trouko.com/questions/questions_else/ما_هي_اسعار_المنازل_في_اوربا/", 
- "namePath" : "ما هي اسعار المنازل في اوربا" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_else/ماذا_حدث_لأسهم_شركة_Game_Stop/", 
- "namePath" : "ماذا حدث لأسهم شركة Game Stop" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_physics/كيف_يعمل_المايكرويف/", 
- "namePath" : "كيف يعمل المايكرويف" 
+ "linkPage" : "https://www.trouko.com/subjects/physics/عالم_الكم_و_الزمان/", 
+ "namePath" : "عالم الكم و الزمان" 
 },{
  "linkPage" : "https://www.trouko.com/subjects/technology/اهم_ثلاث_مواقع_لتعليم_البرمجة/", 
  "namePath" : "اهم ثلاث مواقع لتعليم البرمجة" 
@@ -53,57 +23,6 @@
 },{
  "linkPage" : "https://www.trouko.com/subjects/technology/هل_لغة_بايثون_مفيدة_حقاً/", 
  "namePath" : "هل لغة بايثون مفيدة حقاً" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_technology/كيفية_تسريع_جهاز_الكمبيوتر_أو_اللابتوب/", 
- "namePath" : "كيفية تسريع جهاز الكمبيوتر أو اللابتوب" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_technology/ما_هو_الشحن_اللاسلكي/", 
- "namePath" : "ما هو الشحن اللاسلكي" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_technology/ما_هي_شاشات_الحبرالالكتروني/", 
- "namePath" : "ما هي شاشات الحبرالالكتروني" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/physics/الطاقة_المتجددة/", 
- "namePath" : "الطاقة المتجددة" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/physics/عالم_الكم_و_الزمان/", 
- "namePath" : "عالم الكم و الزمان" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/أقرب_كوكب_صالح_للحياة_من_الأرض/", 
- "namePath" : "أقرب كوكب صالح للحياة من الأرض" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/كم_عدد_النجوم_في_مجرتنا/", 
- "namePath" : "كم عدد النجوم في مجرتنا" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/كم_عمر_مجرة_درب_التبانة/", 
- "namePath" : "كم عمر مجرة درب التبانة" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/كيف_استطاعات_ناسا_من_تحويل_غاز_ثاني_اكسيد_الكاربون_الى_اكسجين/", 
- "namePath" : "كيف استطاعات ناسا من تحويل غاز ثاني اكسيد الكاربون الى اكسجين" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/كيف_نشأت_مجرة_درب_التبانة/", 
- "namePath" : "كيف نشأت مجرة درب التبانة" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هو_تاريخ_الأشعة_الكونية/", 
- "namePath" : "ما هو تاريخ الأشعة الكونية" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هو_عمر_الكون/", 
- "namePath" : "ما هو عمر الكون" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هي_أنواع_المجرات/", 
- "namePath" : "ما هي أنواع المجرات" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هي_الطاقة_المضلمة/", 
- "namePath" : "ما هي الطاقة المضلمة" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هي_خصائص_الحياة_على_سطح_المريخ/", 
- "namePath" : "ما هي خصائص الحياة على سطح المريخ" 
-},{
- "linkPage" : "https://www.trouko.com/questions/questions_space/متى_سيكون_المريخ_صالح_للعيش/", 
- "namePath" : "متى سيكون المريخ صالح للعيش" 
-},{
- "linkPage" : "https://www.trouko.com/subjects/space/ass/", 
- "namePath" : "ass" 
 },{
  "linkPage" : "https://www.trouko.com/subjects/space/أقمار_كوكب_المريخ/", 
  "namePath" : "أقمار كوكب المريخ" 
@@ -140,5 +59,83 @@
 },{
  "linkPage" : "https://www.trouko.com/subjects/space/هل_وصلت_ناسا_الى_المريخ_حقاً/", 
  "namePath" : "هل وصلت ناسا الى المريخ حقاً" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_else/البورصه/", 
+ "namePath" : "البورصه" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_else/ما_هي_اسعار_المنازل_في_اوربا/", 
+ "namePath" : "ما هي اسعار المنازل في اوربا" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_else/ماذا_حدث_لأسهم_شركة_Game_Stop/", 
+ "namePath" : "ماذا حدث لأسهم شركة Game Stop" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/أقرب_كوكب_صالح_للحياة_من_الأرض/", 
+ "namePath" : "أقرب كوكب صالح للحياة من الأرض" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/كم_عدد_النجوم_في_مجرتنا/", 
+ "namePath" : "كم عدد النجوم في مجرتنا" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/كم_عمر_مجرة_درب_التبانة/", 
+ "namePath" : "كم عمر مجرة درب التبانة" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/كيف_استطاعات_ناسا_من_تحويل_غاز_ثاني_اكسيد_الكاربون_الى_اكسجين/", 
+ "namePath" : "كيف استطاعات ناسا من تحويل غاز ثاني اكسيد الكاربون الى اكسجين" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/كيف_نشأت_مجرة_درب_التبانة/", 
+ "namePath" : "كيف نشأت مجرة درب التبانة" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هو_تاريخ_الأشعة_الكونية/", 
+ "namePath" : "ما هو تاريخ الأشعة الكونية" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هو_عمر_الكون/", 
+ "namePath" : "ما هو عمر الكون" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هي_أنواع_المجرات/", 
+ "namePath" : "ما هي أنواع المجرات" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هي_الطاقة_المضلمة/", 
+ "namePath" : "ما هي الطاقة المضلمة" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/ما_هي_خصائص_الحياة_على_سطح_المريخ/", 
+ "namePath" : "ما هي خصائص الحياة على سطح المريخ" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_space/متى_سيكون_المريخ_صالح_للعيش/", 
+ "namePath" : "متى سيكون المريخ صالح للعيش" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_physics/كيف_يعمل_المايكرويف/", 
+ "namePath" : "كيف يعمل المايكرويف" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_technology/كيفية_تسريع_جهاز_الكمبيوتر_أو_اللابتوب/", 
+ "namePath" : "كيفية تسريع جهاز الكمبيوتر أو اللابتوب" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_technology/ما_هو_الشحن_اللاسلكي/", 
+ "namePath" : "ما هو الشحن اللاسلكي" 
+},{
+ "linkPage" : "https://www.trouko.com/questions/questions_technology/ما_هي_شاشات_الحبرالالكتروني/", 
+ "namePath" : "ما هي شاشات الحبرالالكتروني" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/أبل/", 
+ "namePath" : "أبل" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/أجمل_دول_العالم/", 
+ "namePath" : "أجمل دول العالم" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/تيسلا_موتورز/", 
+ "namePath" : "تيسلا موتورز" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/لا_تنخدع_بالربح_من_الانترنت/", 
+ "namePath" : "لا تنخدع بالربح من الانترنت" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/ما_هي_اسعار_المنازل_في_كندا/", 
+ "namePath" : "ما هي اسعار المنازل في كندا" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/مشروع_أبل_لصناعة_السيارات/", 
+ "namePath" : "مشروع أبل لصناعة السيارات" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/مقارنة_بين_السيارات_التي_تعمل_بالكهرباء_و_التي_تعمل_بالبانزين/", 
+ "namePath" : "مقارنة بين السيارات التي تعمل بالكهرباء و التي تعمل بالبانزين" 
+},{
+ "linkPage" : "https://www.trouko.com/subjects/else/هل_الفيس_بوك_مربح_حقاً/", 
+ "namePath" : "هل الفيس بوك مربح حقاً" 
 }
 ]
