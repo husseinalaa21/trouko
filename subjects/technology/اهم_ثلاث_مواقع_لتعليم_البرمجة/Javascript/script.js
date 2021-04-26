0dff4f0879c27a7e7ec2b729a6166433167f3203
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-26 14:39:38 +0300

    F

--- subjects/technology/اهم_ثلاث_مواقع_لتعليم_البرمجة/Javascript/script.js
+++ subjects/technology/اهم_ثلاث_مواقع_لتعليم_البرمجة/Javascript/script.js
@@ -14,9 +14,9 @@ var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_
     addSourcesAndQuotesfive_na = "";
     addSourcesAndQuotesSix_na = "";
     addSourcesAndQuotesSeven_na
-    addSugg_vrOne  = "";
-    addSugg_vrTwo  = "";
-    addSugg_vrThree  = "";
+    addSugg_vrOne  = "https://trouko.com/subjects/else/لا_تنخدع_بالربح_من_الانترنت/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/else/هل_الفيس_بوك_مربح_حقاً/";
+    addSugg_vrThree  = "https://trouko.com/subjects/technology/هل_لغة_بايثون_مفيدة_حقاً/";
     addSugg_vrFour  = "";
     addSugg_vrFive = "";
     namePpath = "https://www.trouko.com/subjects/technology/اهم_ثلاث_مواقع_لتعليم_البرمجة";
