8654f9ec448bcd25b93d9bb795f08b394a6e1d70
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-27 00:10:45 +0300

    G

--- container_s_q/script.js
+++ container_s_q/script.js
@@ -1,8 +1,11 @@
-var xReq = new XMLHttpRequest();
-xReq.open('GET','../data.js');
+var someEncodedString = encodeURI(window.location.href)
+document.getElementById("sheTwi").innerHTML = '<a href="http://twitter.com/share?text=' + document.getElementById("titleTs").innerText + '&url=' + someEncodedString + '" target="_blank" class="sheThPage twitShe"><img src="../../../icons/twitter-m.svg" width="13px" height="13px"/>'
 
-function randoPag(){
+var xReq = new XMLHttpRequest();
+xReq.open('GET', '../data.js');
+
+function randoPag() {
     var xData = JSON.parse(xReq.responseText);
-    var r = Math.floor(Math.random() * xData.length) 
+    var r = Math.floor(Math.random() * xData.length)
     location.href = xData[r].linkPage
 }
