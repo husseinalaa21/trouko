50c5391237a9a2911a08274559e9461ba68f17a6
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-30 00:36:25 +0300

    R

---
+++ subjects/space/كم_تكلفة_ارسال_قمر_اصطناعي_الى_المدار_الارضي/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "كم تكلفة ارسال قمر اصطناعي الى المدار الارضي";
+adPhoto = "../../../imgs/satellite40.jpg"
+adDiscription = "يتوجه العالم في الوقت الحالي الى الاعتماد على تكنلوجيا الاتصالات , الامر الذي يدعو الكثير من الشركات و الدول الى الاعتماد على الاقمار الاصطناعية بشكل اساسي . مما جعل الشركات  المتخصصة في مجال الصواريخ و الفضاء الى التنافس لجعل الانتقال الفضاء و عبور المدار الارض امراً سهل , فمثلا شركة سبيس امضت سبع سنوات تقريباً لخلق وسيلة  تمكنهم من الذهاب الى الفضاء و قذف الحمولة ثم المعاودة الى الارض ولا شكل أنها نجحت في ذلك . و بالطبع يوجد العديد من الشركات ووكالات الطياران تنافس شركة سبيس في ايصال الاقمار الاصطناعي او الحمولة الى الفضاء الخارجي ."
+namePpath = "https://www.trouko.com/subjects/space/كم_تكلفة_ارسال_قمر_اصطناعي_الى_المدار_الارضي/"
+infSuGuPageOn = '<p>  المواضيع / الفضاء </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الجمعه, 30 ابريل 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://www.trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
---
+++ subjects/space/كم_تكلفة_ارسال_قمر_اصطناعي_الى_المدار_الارضي/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "spacenews";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "https://spacenews.com/op-ed-can-spacex-profit-on-certain-starlink-launches/";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/space/هل_وصلت_ناسا_الى_المريخ_حقاً/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/space/هل_سوف_تتجه_شركة_سبيس_أكس_للرحلات_السياحية/";
+    addSugg_vrThree  = "https://trouko.com/subjects/space/ما_المدة_الزمنية_المستغرقة_للذهاب_الى_القمر/";
+    addSugg_vrFour  = "https://trouko.com/subjects/space/سبيس_اكس/";
+    addSugg_vrFive = "https://trouko.com/subjects/space/ناسا/";
+    namePpath = "https://www.trouko.com/subjects/space/كم_تكلفة_ارسال_قمر_اصطناعي_الى_المدار_الارضي";
