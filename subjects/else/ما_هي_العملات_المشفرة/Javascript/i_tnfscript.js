935bfc19af1e3958f0efea703d7a57c6d798439b
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-29 21:17:08 +0300

    F

---
+++ subjects/else/ما_هي_العملات_المشفرة/Javascript/i_tnfscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "ما هي العملات المشفرة";
+adPhoto = "../../../imgs/cryptocurre.jpg"
+adDiscription = "شهدت الاعوام القليلة الماضية تطور هائل في مجال العملات الرقمية , و شهدت ايضاً ارتفاع هائل في سعرها . و ارتفعت اسعار العملات الرقمية بشكل مأهول مما جعلها تكون أغلى من الدولار و حتى الذهب . ومن أشهر العملات الرقمية التي شهدت ارتفاع كبير في سعرها هي البيتكوين و الايثيريوم مؤخراً . مما جعل الكثير من الاشخاص يعتمدون على الربح منها , فهل العملات المشفرة أو ' العملات الرقمية ' فيها خطورة ؟, وما هي ؟"
+namePpath = "https://www.trouko.com/subjects/else/ما_هي_العملات_المشفرة/"
+infSuGuPageOn = '<p>  المواضيع / متنوعة   </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الخميس, 29 ابريل 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://www.trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_s_262');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
---
+++ subjects/else/ما_هي_العملات_المشفرة/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/else/كيفية_شراء_اسهم_من_الشركة/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/else/لا_تنخدع_بالربح_من_الانترنت/";
+    addSugg_vrThree  = "https://trouko.com/subjects/else/مشروع_أبل_لصناعة_السيارات/";
+    addSugg_vrFour  = "https://trouko.com/questions/questions_else/البورصه/";
+    addSugg_vrFive = "https://trouko.com/questions/questions_else/ماذا_حدث_لأسهم_شركة_Game_Stop/";
+    namePpath = "https://www.trouko.com/subjects/else/ما_هي_العملات_المشفرة";
