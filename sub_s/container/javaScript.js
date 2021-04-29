456f454ea0db917a768fbb240019ac7bd78d179a
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-29 17:57:25 +0300

    F

--- sub_s/container/javaScript.js
+++ sub_s/container/javaScript.js
@@ -118,3 +118,48 @@ function addNewSubrt(m, xz, vz, xxc, psssp, classP) {
         pMine.append(li)
     });
 }
+var iconT = document.getElementById("uMenuMine_m"),
+    iconBarTh = document.getElementsByClassName("icon-bar")[0],
+    iconBar = document.getElementsByClassName("icon-bar")[1],
+    iconBarT = document.getElementsByClassName("icon-bar")[2],
+    isClickMenu = false,
+    menLis = document.getElementById("menuLisWeb");
+
+function copThis() {
+}
+function shoLiThisPage() {
+    document.getElementById("rlsh").innerHTML = `<div class="resLinShar"> <div onclick="copThis()"> <img src="../../../icons/link-solid.svg" class="linSharICon"/> </div> <div class="sharLinContainer" >`+window.location.href + `</div> </div>`
+}
+var lisBox = `<div class="lisBox">
+                <div class="toLisBox">
+                    <div class="secMainLis" > <a href="/"> الصفحة الرئيسية </a> </div>
+                    <div class="secSecLis" > <a href="../../../container_s_q/about.html"> حول </a></div>
+                </div>
+                <div class="medLis">
+                    <a class="fasMaenu" href="https://web.facebook.com/%D8%AA%D8%B1%D9%88%D9%83%D9%88-trouko-104470178026820"> <img src="../../../icons/facebook-m.svg" class="fasImg" /> </a>
+                    <a class="twiMaenu" href="https://twitter.com/trouko1"> <img src="../../../icons/twitter-m.svg" /> </a>
+                    <a class="telegramMaenu" href="https://t.me/trouko"> <img src="../../../icons/telegram-m.svg" /> </a>
+                </div>
+                <div class="endlis" onclick="shoLiThisPage()">
+                    <div><img src='../../../icons/external-link-square-alt-solid.svg' class="icoLinShar"/></div><div class="conSharLin"> مشاركة هذه المقالة </div>
+                </div>
+                <div id="rlsh" class="linContainer"></div>
+            </div>`
+iconT.addEventListener("click", e => {
+    if (isClickMenu === false) {
+        iconBarTh.className = "icon-bar bar_a";
+        setTimeout(function () { iconBar.className = "icon-bar bar_a" }, 150);
+        setTimeout(function () { iconBarT.className = "icon-bar bar_a" }, 250);
+        menLis.innerHTML = lisBox
+        isClickMenu = true
+    } else {
+        cloLis()
+    }
+})
+function cloLis(){
+    iconBarTh.className = "icon-bar";
+    iconBar.className = "icon-bar"
+    iconBarT.className = "icon-bar"
+    menLis.innerHTML = ""
+    isClickMenu = false
+}
