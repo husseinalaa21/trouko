8728dd595c7ba1453e59e7365923462f06c69fac
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-30 01:42:56 +0300

    R

---
+++ subjects/technology/اهم_مواصفات_ايفون_13/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "اهم مواصفات ايفون 13";
+adPhoto = "../../../imgs/phone-801_640.jpg"
+adDiscription = "تعد شركة ابل من اكثر الشركات التي تقدمت في مجال صناعة الهواتف بل و تتصدر هواتفها في كل مرة قائمة افضل الهواتف العالمية , ويجدر الذكر أنه كان اول هاتف للشركة في عام 2007  و منذ اطلاق شركة ابل اول هواتفها و هي تتصدر في استمرار في كل مرة ."
+namePpath = "https://www.trouko.com/subjects/technology/اهم_مواصفات_ايفون_13/"
+infSuGuPageOn = '<p>  المواضيع / تكنلوجيا </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الجمعه  , 30 ابريل 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://www.trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
