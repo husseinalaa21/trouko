61ff3ba376a173bd823c7cd4639136be8e761ab3
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-30 02:05:13 +0300

    R

---
+++ questions/questions_physics/هل_يوجد_شيء_اسرع_من_الضوء/Javascript/i_tnfscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "هل يوجد شيء اسرع من الضوء";
+adPhoto = "../../../imgs/night-photograph.jpg"
+adDiscription = "سرعة الضوء في الفضاء هي ثابت فيزيائي يرمز له في العادة بالرمز  ' c ' , وتساوي سرعة الضوء 299,792,458 متر لكل ثانية  , وبحسب قوانياً النسبية الخاصة التي طرحها العالم الفيزيائي ' آلبرت آينشتاين ' في عام 1905 أن سرعة الضوء هي أقصى سرعة تستطيع أن تسافر بها كل أشكال الطاقة أو المادة . كما وأنها  سرعة سفر الجسيمات عديمة الكتلة ومجالاتها المتلازمة (بما في ذلك الإشعاع الكهرومغناطيسي مثل الضوء) عبر الفراغ . وبحسب القوانين الفيزيائية للكون أنه لا توجد سرعة اسرع من الضوء بل و اذا افترضاً أن جسم ما استطاع أن يسبق سرعة الضوء ; فسوف يحترق على الفور ويتلاشى .<br />"
+namePpath = "https://www.trouko.com/questions/questions_physics/هل_يوجد_شيء_اسرع_من_الضوء/"
+infSuGuPageOn = '<p>  أسئلة / الفيزياء  </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الجمعه  , 30 ابريل 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://www.trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_s_262');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
---
+++ questions/questions_physics/هل_يوجد_شيء_اسرع_من_الضوء/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/physics/الطاقة_المتجددة/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/space/هل_وصلت_ناسا_الى_المريخ_حقاً/";
+    addSugg_vrThree  = "https://trouko.com/subjects/space/سبيس_اكس/";
+    addSugg_vrFour  = "https://trouko.com/subjects/space/تضاريس_كوكب_المريخ/";
+    addSugg_vrFive = "https://trouko.com/subjects/space/هل_وصلت_ناسا_الى_المريخ_حقاً/";
+    namePpath = "https://www.trouko.com/questions/questions_physics/هل_يوجد_شيء_اسرع_من_الضوء";
