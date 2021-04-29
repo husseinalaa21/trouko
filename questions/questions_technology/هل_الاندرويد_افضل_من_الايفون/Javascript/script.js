ad81473f6af2a9d0a53c181519756ed6d96f7c12
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-30 01:03:56 +0300

    R

---
+++ questions/questions_technology/هل_الاندرويد_افضل_من_الايفون/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/technology/أفضل_و_ارخص_سيارة_كهربائية_لهذا_العام/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/technology/ما_هو_مستقبل_الذكاء_الاصطناعي/";
+    addSugg_vrThree  = "https://trouko.com/subjects/technology/هل_سوف_تتحول_أفلام_الخيال_العلمي_الى_حقيقة_؟/";
+    addSugg_vrFour  = "https://trouko.com/questions/questions_technology/كيفية_تسريع_جهاز_الكمبيوتر_أو_اللابتوب/";
+    addSugg_vrFive = "https://trouko.com/questions/questions_technology/ما_هي_شاشات_الحبرالالكتروني/";
+    namePpath = "https://www.trouko.com/questions/questions_technology/هل_الاندرويد_افضل_من_الايفون";
