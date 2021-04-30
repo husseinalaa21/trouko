939f865d0e83ff4eb2a9466ddad42937409779d5
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-04-30 03:51:41 +0300

    R

--- questions/questions_space/أقرب_كوكب_صالح_للحياة_من_الأرض/Javascript/script.js
+++ questions/questions_space/أقرب_كوكب_صالح_للحياة_من_الأرض/Javascript/script.js
@@ -1,22 +1,22 @@
 var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
     addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
-    addSourcesAndQuotesOne_n = "ويكيبديا الموسوعة الحرة";
+    addSourcesAndQuotesOne_n = " ويكيبديا الموسوعة الحرة";
     addSourcesAndQuotesTwo_n = "";
     addSourcesAndQuotesThree_n = "";
     addSourcesAndQuotesFour_n = "";
     addSourcesAndQuotesfive_n = "";
     addSourcesAndQuotesSix_n = "";
     addSourcesAndQuotesSeven_n
-    addSourcesAndQuotesOne_na = "https://ar.wikipedia.org/wiki/%D9%82%D9%86%D8%B7%D9%88%D8%B1_%D8%A7%D9%84%D8%A3%D9%82%D8%B1%D8%A8_b";
+    addSourcesAndQuotesOne_na = "";
     addSourcesAndQuotesTwo_na = "";
     addSourcesAndQuotesThree_na = "";
     addSourcesAndQuotesFour_na = "";
     addSourcesAndQuotesfive_na = "";
     addSourcesAndQuotesSix_na = "";
     addSourcesAndQuotesSeven_na
-    addSugg_vrOne  = "";
-    addSugg_vrTwo  = "";
-    addSugg_vrThree  = "";
-    addSugg_vrFour  = "";
-    addSugg_vrFive = "";
+    addSugg_vrOne  = "https://trouko.com/subjects/space/استعمار_المريخ/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/space/أقمار_كوكب_المريخ/";
+    addSugg_vrThree  = "https://trouko.com/subjects/space/تضاريس_كوكب_المريخ/";
+    addSugg_vrFour  = "https://trouko.com/subjects/space/كم_تكلفة_ارسال_قمر_اصطناعي_الى_المدار_الارضي/";
+    addSugg_vrFive = "https://trouko.com/subjects/space/ناسا_تصنع_المستحيل_و_تحول_ثاني_اكسيد_الكاربون_الى_اوكسجين_نقي/";
     namePpath = "https://www.trouko.com/questions/questions_space/أقرب_كوكب_صالح_للحياة_من_الأرض";
