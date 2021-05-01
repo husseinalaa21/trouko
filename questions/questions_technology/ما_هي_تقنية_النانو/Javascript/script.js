596e7da56b70a5be24e22d1f690c6a0bd6b5fe45
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-05-01 22:31:08 +0300

    R

---
+++ questions/questions_technology/ما_هي_تقنية_النانو/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/physics/ماذا_يحدث_عند_ارتفاع_درجة_حرارة_الاجسام/";
+    addSugg_vrTwo  = "https://trouko.com/questions/questions_physics/هل_يوجد_شيء_اسرع_من_الضوء/";
+    addSugg_vrThree  = "https://trouko.com/questions/questions_physics/كيف_يعمل_المايكرويف/";
+    addSugg_vrFour  = "https://trouko.com/subjects/space/كم_تكلفة_ارسال_قمر_اصطناعي_الى_المدار_الارضي/";
+    addSugg_vrFive = "https://trouko.com/subjects/space/هل_سوف_تتجه_شركة_سبيس_أكس_للرحلات_السياحية/";
+    namePpath = "https://trouko.com/questions/questions_technology/ما_هي_تقنية_النانو";
