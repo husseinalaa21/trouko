6342db5e307f179b911fd941db6aa5d5426c1044
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-05-02 23:23:46 +0300

    F

---
+++ subjects/space/ما_هو_المسبار_الفضائي/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "ما هو المسبار الفضائي";
+adPhoto = "../../../imgs/hubble-telescope-0.jpg"
+adDiscription = "يستخدم المسبار الفضائي لاستكشاف الفضاء الخارجي , و يكون على شكل مركبة فضائي ولكن بدون طاقم ' الية التحكم ' . وتكون حموله المسبار الفضائي المعدات المستخدمة للتواصل و ارسال البيانات الى الارض , بالاضافة الى كامرات عالية الدقة و مستشعرات و  أجهزة المطياف، مقياس الطاقة الإشعاعية، ومقياس المغناطيسية و ما الى ذلك ."
+namePpath = "https://trouko.com/subjects/space/ما_هو_المسبار_الفضائي/"
+infSuGuPageOn = '<p>  المواضيع / الفضاء </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الاحد, 2 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
