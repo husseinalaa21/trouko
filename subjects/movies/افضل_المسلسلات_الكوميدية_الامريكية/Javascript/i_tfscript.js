984516190e7a268b5b641341b2cbb8a0183f1ca2
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-05-04 02:45:33 +0300

    R

---
+++ subjects/movies/افضل_المسلسلات_الكوميدية_الامريكية/Javascript/i_tfscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "افضل المسلسلات الكوميدية الامريكية";
+adPhoto = "../../../imgs/theoffic.jpeg"
+adDiscription = "يوجد العديد من المسلسلات الكوميدية الامريكية , وفي هذه المقالة اخترنا بعض من هذه المسلسلات و التي اشتهرت بكثرة ."
+namePpath = "https://trouko.com/subjects/movies/افضل_المسلسلات_الكوميدية_الامريكية/"
+infSuGuPageOn = '<p>  المواضيع / افلام و مسلسلات   </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الثلاثاء, 4 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_x_242');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
