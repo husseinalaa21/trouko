d186610aa03dd6daa61fdb0e93e86e46518cf009
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-05-09 00:07:22 +0300

    b

---
+++ subjects/technology/ما_هو_الانترنت/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "HubSpot";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "https://blog.hubspot.com/marketing/google-search-statistics";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/technology/افضل_شركات_تصنيع_الكمبيوترات_و_اللابتوبات/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/technology/ما_هو_مستقبل_الذكاء_الاصطناعي/";
+    addSugg_vrThree  = "https://trouko.com/subjects/technology/هل_سوف_تتحول_أفلام_الخيال_العلمي_الى_حقيقة_؟/";
+    addSugg_vrFour  = "https://trouko.com/subjects/technology/هل_لغة_بايثون_مفيدة_حقاً/";
+    addSugg_vrFive = "https://trouko.com/questions/questions_technology/ما_هي_تقنية_النانو/";
+    namePpath = "https://trouko.com/subjects/technology/ما_هو_الانترنت";
