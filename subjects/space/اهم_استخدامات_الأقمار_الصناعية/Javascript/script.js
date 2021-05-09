9c0b4babea78da8949d117b1bf874bdbecc25a05
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-05-09 21:52:15 +0300

    b

---
+++ subjects/space/اهم_استخدامات_الأقمار_الصناعية/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/technology/ما_هو_الانترنت/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/technology/ما_هو_مستقبل_الذكاء_الاصطناعي/";
+    addSugg_vrThree  = "https://trouko.com/subjects/technology/ما_هي_شريحة_نيورالينك/";
+    addSugg_vrFour  = "https://trouko.com/subjects/technology/هل_سوف_تتحول_أفلام_الخيال_العلمي_الى_حقيقة_؟/";
+    addSugg_vrFive = "https://trouko.com/questions/questions_space/متى_سيكون_المريخ_صالح_للعيش/";
+    namePpath = "https://trouko.com/subjects/space/اهم_استخدامات_الأقمار_الصناعية";
