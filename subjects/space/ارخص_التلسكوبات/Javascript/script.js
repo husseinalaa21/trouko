0473e2ecee2ea5b0a8eb5fec04b582b5b98b68ce
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-14 22:56:50 +0300

    F

---
+++ subjects/space/ارخص_التلسكوبات/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/space/احدث_التلسكوبات/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/space/ما_هو_المسبار_الفضائي/";
+    addSugg_vrThree  = "https://trouko.com/subjects/space/مركبة_سبيس_إكس_دراجون/";
+    addSugg_vrFour  = "https://trouko.com/subjects/space/ناسا_تصنع_المستحيل_و_تحول_ثاني_اكسيد_الكاربون_الى_اوكسجين_نقي/";
+    addSugg_vrFive = "https://trouko.com/subjects/space/بعثة_مأهولة_قادمة_الى_القمر_بحلول_عام_2024/";
+    namePpath = "https://trouko.com/subjects/space/ارخص_التلسكوبات";
