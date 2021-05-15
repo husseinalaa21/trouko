45197d81c2d9f89a0a08817edde6f707bd855883
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-15 18:24:48 +0300

    F

---
+++ subjects/sciences/ما_هي_فروع_علم_الوراثة/Javascript/i_tfscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "ما هي فروع علم الوراثة";
+adPhoto = "../../../imgs/Genetics.jpg"
+adDiscription = "يعد علم الوراثة العلم الذي يدرس المورثات ' الجينات ' والوراثة وما ينتج عنه من تنوع الكائنات الحية . ويعد علم الوراثة الحديث العلم الذي يحاول فهم آلية توريث الصفات . و قام العالم غريغور مندل ' الذي يعد والد علم الوراثة ' بمراقبة الصفات الموروثة للكائنات الحية وكيفية انتقالها من الآباء إلى الأبناء ي منتصف القرن التاسع عشر ."
+namePpath = "https://trouko.com/subjects/sciences/ما_هي_فروع_علم_الوراثة/"
+infSuGuPageOn = '<p>  المواضيع / العلوم   </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> السبت, 15 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_x_242');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
---
+++ subjects/sciences/ما_هي_فروع_علم_الوراثة/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/questions/questions_space/أقرب_كوكب_صالح_للحياة_من_الأرض/";
+    addSugg_vrTwo  = "https://trouko.com/questions/questions_space/ما_هو_عمر_الكون/";
+    addSugg_vrThree  = "https://trouko.com/questions/questions_space/ما_هي_أنواع_المجرات/";
+    addSugg_vrFour  = "https://trouko.com/questions/questions_space/كم_عمر_مجرة_درب_التبانة/";
+    addSugg_vrFive = "https://trouko.com/subjects/space/احدث_التلسكوبات/";
+    namePpath = "https://trouko.com/subjects/sciences/ما_هي_فروع_علم_الوراثة";
