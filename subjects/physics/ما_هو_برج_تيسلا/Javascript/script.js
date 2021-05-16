d41069607611773165e504fc44540f678476ffa9
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-16 20:21:45 +0300

    F

---
+++ subjects/physics/ما_هو_برج_تيسلا/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/physics/ما_هو_مقياس_الجهد_الكهربائي/";
+    addSugg_vrTwo  = "https://trouko.com/questions/questions_physics/كيف_يعمل_المايكرويف/";
+    addSugg_vrThree  = "https://trouko.com/questions/questions_physics/ما_هي_طرق_توليد_الكهرباء/";
+    addSugg_vrFour  = "https://trouko.com/questions/questions_physics/هل_يوجد_شيء_اسرع_من_الضوء/";
+    addSugg_vrFive = "";
+    namePpath = "https://trouko.com/subjects/physics/ما_هو_برج_تيسلا";
