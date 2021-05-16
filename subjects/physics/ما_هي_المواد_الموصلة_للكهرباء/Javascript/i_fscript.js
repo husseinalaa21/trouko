f06becabf4fcd3a1f9e8000870da73489fd0764c
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-16 20:52:10 +0300

    F

---
+++ subjects/physics/ما_هي_المواد_الموصلة_للكهرباء/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "ما هي المواد الموصلة للكهرباء";
+adPhoto = "../../../imgs/electricity-access.jpg"
+adDiscription = "المواد الموصلة للكهرباء هي اجسام قادره على تمرير تيار كهربائي. في كثير من الأحيان، و بشكل عام تكون المعادن هي الموصلة كهربائياً، و تعد الفضة والنحاس والذهب هي المواد الاكثر ايصلاً للكهرباء . وفي هذه المقالة سوف نتحدث المواد الموصلة للكهرباء بشكل عام ."
+namePpath = "https://trouko.com/subjects/physics/ما_هي_المواد_الموصلة_للكهرباء/"
+infSuGuPageOn = '<p>  المواضيع / الفيزياء </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الاحد, 16 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
---
+++ subjects/physics/ما_هي_المواد_الموصلة_للكهرباء/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/questions/questions_physics/ما_هي_طرق_توليد_الكهرباء/";
+    addSugg_vrTwo  = "https://trouko.com/questions/questions_else/هل_البيوت_الخشبية_أفضل_من_بيوت_الاسمنت/";
+    addSugg_vrThree  = "https://trouko.com/subjects/else/ما_هو_المستقبل_البترولي/";
+    addSugg_vrFour  = "https://trouko.com/subjects/else/ما_هي_مزرعة_الرياح_أو_طاقة_الرياح/";
+    addSugg_vrFive = "https://trouko.com/subjects/else/تيسلا_موتورز/";
+    namePpath = "https://trouko.com/subjects/physics/ما_هي_المواد_الموصلة_للكهرباء";
