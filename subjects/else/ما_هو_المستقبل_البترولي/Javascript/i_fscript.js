907acc4567613521044f6ab23479e0c97a9bf0db
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-16 15:50:47 +0300

    F

---
+++ subjects/else/ما_هو_المستقبل_البترولي/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "ما هو المستقبل البترولي";
+adPhoto = "../../../imgs/petroleum.jpg"
+adDiscription = "يعد البترول مصدر لإنتاج الطاقة منذ القرن التاسع عشر. تم اقتراح مفهوم ذروة النفط من قبل نظرية إم كينج هوبرت . و لطالما لعب البترول دورًا حيويًا في التاريخ الاقتصادي والاجتماعي والسياسي للعالم.<br />أن ذروة النفط لا تعني نفاد احتياطيات البترول ، ولكن تم الوصول إلى الحد الأقصى لمعدل استخراج البترول وأن طرق الاستخراج اللاحقة لا يمكن أن تزيد المعدل أكثر. وبالتالي سينخفض ​​المعدل الإجمالي لإنتاج البترول. هذه الظاهرة تشكل خطرا على مستقبل البترول. نظرًا لتناقص إمدادات النفط الخام التي يمكن استخراجها بسهولة وزيادة الطلب ، كما و أصبح استخراج النفط الثقيل الذي كان من غير المجدي اقتصاديًا لاستخراجه بسبب التكاليف الأولية المرتفعة مربحًا للإنتاج. نظرًا لأن 70٪ من إجمالي النفط الخام المنتج تستخدمه صناعة النقل ، فقد تم بذل جهد كبير لإنتاج سيارة كهربائية قادرة على أداء مشابه لأداء المركبات التي تعمل بالبنزين. <br />ويعد العامل الرئيسي الوحيد الذي يعيق السيارات الكهربائية هو كيفية تخزينها للكهرباء عندما لا تكون السيارة قيد الاستخدام. يمكن للسيارات الكهربائية أن تقلل من استخدام البترول ، ولكن النقطة البارزة هي مصدر الكهرباء المستخدمة لشحن بطارياتها. "
+namePpath = "https://trouko.com/subjects/else/ما_هو_المستقبل_البترولي/"
+infSuGuPageOn = '<p>  المواضيع / متنوعة   </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الاحد  , 16 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
---
+++ subjects/else/ما_هو_المستقبل_البترولي/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/physics/الطاقة_المتجددة/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/physics/ما_هو_مقياس_الجهد_الكهربائي/";
+    addSugg_vrThree  = "https://trouko.com/subjects/physics/ما_هي_الألواح_الشمسية/";
+    addSugg_vrFour  = "https://trouko.com/subjects/physics/ما_هي_الطاقة_الحركية/";
+    addSugg_vrFive = "https://trouko.com/subjects/physics/ماذا_يحدث_عند_ارتفاع_درجة_حرارة_الاجسام/";
+    namePpath = "https://trouko.com/subjects/else/ما_هو_المستقبل_البترولي";
