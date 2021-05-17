d29c377de3ad3a890e2246df8c9990e32d3a5397
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-18 01:11:55 +0300

    F

--- index_js/search_sub.js
+++ index_js/search_sub.js
@@ -106,44 +106,44 @@ function savDev(x) {
                     nCalln ++;
                     serIn()
                 })
-            } else {
-                if (robenAre) clearTimeout(robenAre);
-                robenAre = setTimeout(() => {
-                    if (catcho.length > 0) {
-                        var lisWeb = [], numCall = 0
-                        seeReels()
-                        function seeReels() {
-                            if (numCall < catcho.length) {
-                                let heNum = 0, n, u, numCatch;
-                                for (var e = 0; e < catcho.length; e++) {
-                                    if (catcho[e].num > heNum) {
-                                        heNum = catcho[e].num
-                                        n = catcho[e].tit
-                                        u = catcho[e].url
-                                        numCatch = e
-                                    }
-                                }
-                                catcho[numCatch] = ""
-                                lisWeb.push({ n: n, u: u })
-                                numCall++;
-                                seeReels()
-                            } else {
-                                lisWeb.forEach(r => {
-                                    let er = document.createElement("p")
-                                    er.innerHTML = `<a href ="` + r.u + `" >` + r.n + `</a>`
-                                    nmDiv.append(er)
-                                })
-                            }
-                        }
-                    } else {
-                        let handel = document.createElement("div")
-                        handel.className = "handlx"
-                        handel.innerHTML = `<div class="handl" >لا توجد نتائج </div>`
-                        nmDiv.append(handel)
-                    }
-                    resSer.append(nmDiv)
-                }, 10);
             }
+        } else {
+            if (robenAre) clearTimeout(robenAre);
+            robenAre = setTimeout(() => {
+                if (catcho.length > 0) {
+                    var lisWeb = [], numCall = 0
+                    seeReels()
+                    function seeReels() {
+                        if (numCall < catcho.length) {
+                            let heNum = 0, n, u, numCatch;
+                            for (var e = 0; e < catcho.length; e++) {
+                                if (catcho[e].num > heNum) {
+                                    heNum = catcho[e].num
+                                    n = catcho[e].tit
+                                    u = catcho[e].url
+                                    numCatch = e
+                                }
+                            }
+                            catcho[numCatch] = ""
+                            lisWeb.push({ n: n, u: u })
+                            numCall++;
+                            seeReels()
+                        } else {
+                            lisWeb.forEach(r => {
+                                let er = document.createElement("p")
+                                er.innerHTML = `<a href ="` + r.u + `" >` + r.n + `</a>`
+                                nmDiv.append(er)
+                            })
+                        }
+                    }
+                } else {
+                    let handel = document.createElement("div")
+                    handel.className = "handlx"
+                    handel.innerHTML = `<div class="handl" >لا توجد نتائج </div>`
+                    nmDiv.append(handel)
+                }
+                resSer.append(nmDiv)
+            }, 10);
         }
     }
 }
