a2d55c73ef65cde0f8b0eef2155a827046d28da1
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-20 01:04:44 +0300

    F

--- subjects/technology/ما_هو_تاريخ_الهواتف_الذكية/Javascript/script.js
+++ subjects/technology/ما_هو_تاريخ_الهواتف_الذكية/Javascript/script.js
@@ -1,13 +1,13 @@
 var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
     addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
-    addSourcesAndQuotesOne_n = "";
+    addSourcesAndQuotesOne_n = "IBM - IBM Simon";
     addSourcesAndQuotesTwo_n = "";
     addSourcesAndQuotesThree_n = "";
     addSourcesAndQuotesFour_n = "";
     addSourcesAndQuotesfive_n = "";
     addSourcesAndQuotesSix_n = "";
     addSourcesAndQuotesSeven_n
-    addSourcesAndQuotesOne_na = "";
+    addSourcesAndQuotesOne_na = "https://en.wikipedia.org/wiki/IBM_Simon";
     addSourcesAndQuotesTwo_na = "";
     addSourcesAndQuotesThree_na = "";
     addSourcesAndQuotesFour_na = "";
