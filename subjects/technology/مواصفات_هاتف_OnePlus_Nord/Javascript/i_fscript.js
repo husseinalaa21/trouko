3c5c094154f12aad2560a566649d39b5eea47c6c
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-22 21:31:44 +0300

    F

---
+++ subjects/technology/مواصفات_هاتف_OnePlus_Nord/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "مواصفات هاتف OnePlus Nord";
+adPhoto = "../../../imgs/OnePlus-Nord.jpg"
+adDiscription = "طرحت شركة OnePlus Nord هاتفها الجديد ليكون أبرز الهواتف في الفئة المتوسطة , يأتي الهاتف مع معالج Snapdragon 765G وشاشة اموليد بمعدل تحديث الـ 90Hz . سنتحدث في هذه المقالة عن مميزات الهاتف و عيبوبه وهل يستحق الشراء ."
+namePpath = "https://trouko.com/subjects/technology/مواصفات_هاتف_OnePlus_Nord/"
+infSuGuPageOn = '<p>  المواضيع / تكنلوجيا </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> السبت, 22 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
