69430e26df523be9b2626f4e390ea49801fbd528
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-23 16:13:18 +0300

    F

--- subjects/technology/مواصفات_هاتف_Nokia_X20/Javascript/i_tfscript.js
+++ subjects/technology/مواصفات_هاتف_Nokia_X20/Javascript/i_tfscript.js
@@ -4,7 +4,7 @@ adPhoto = "../../../imgs/nokiax20.jpg"
 adDiscription = "تم إطلاق الهاتف الذكي Nokia X20 في 8 أبريل 2021. و يأتي الهاتف بشاشة تعمل باللمس بمقاس 6.67 بوصة بدقة 1080 × 2400 بكسل كما ويأتي هاتف Nokia X20 بمعالج ثماني النواة Qualcomm Snapdragon 480. يأتي ايضاً مع 6 جيجابايت من ذاكرة الوصول العشوائي. يعمل هاتف Nokia X20 بنظام Android 11 ويتم تشغيله بواسطة بطارية تبلغ 4470 مللي أمبير في الساعة."
 namePpath = "https://trouko.com/subjects/technology/مواصفات_هاتف_Nokia_X20/"
 infSuGuPageOn = '<p>  المواضيع / تكنلوجيا </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
-infSuGuPageTw = '<p> السبت  , 22 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+infSuGuPageTw = '<p> السبت, 22 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
 nameP = namePpath.replace('https://trouko.com/',"")
 addTitleIpage = document.getElementById(nameP+'_pag_x_242');
 addt(titleIn)
