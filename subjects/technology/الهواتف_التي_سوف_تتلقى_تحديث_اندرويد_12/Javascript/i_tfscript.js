e67ab3203a5c0bd18add0b97514822df3eba080f
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-24 22:26:49 +0300

    F

--- subjects/technology/الهواتف_التي_سوف_تتلقى_تحديث_اندرويد_12/Javascript/i_tfscript.js
+++ subjects/technology/الهواتف_التي_سوف_تتلقى_تحديث_اندرويد_12/Javascript/i_tfscript.js
@@ -1,10 +1,10 @@
 var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
 titleIn = "الهواتف التي سوف تتلقى تحديث اندرويد 12";
-adPhoto = "../../../imgs/android12.png"
+adPhoto = "../../../imgs/android-12-logo.jpg"
 adDiscription = "في ما يلي قائمة الهواتف التي سوف تتلقى تحديث اندرويد 12 ."
 namePpath = "https://trouko.com/subjects/technology/الهواتف_التي_سوف_تتلقى_تحديث_اندرويد_12/"
 infSuGuPageOn = '<p>  المواضيع / تكنلوجيا </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
-infSuGuPageTw = '<p> الاثنين  , 24 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+infSuGuPageTw = '<p> الاثنين, 24 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
 nameP = namePpath.replace('https://trouko.com/',"")
 addTitleIpage = document.getElementById(nameP+'_pag_x_242');
 addt(titleIn)
