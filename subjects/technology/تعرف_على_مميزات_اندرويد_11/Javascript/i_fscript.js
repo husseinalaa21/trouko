73f15e23e6d7b669d094406dd467b09d352c31d2
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-24 15:37:28 +0300

    F

---
+++ subjects/technology/تعرف_على_مميزات_اندرويد_11/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "تعرف على مميزات اندرويد 11";
+adPhoto = "../../../imgs/android11ed.png"
+adDiscription = "Android 11 هو نظام تشغيل من Google أعلنت عنه Google في 8 سبتمبر 2020 . يأتي هذا الاصدار من الاندرويد شبيه جداً بالاندرويد 10 مع وجود تحسينات في شريط الاشعارات و امكانية الوصول و ايضاً الأمن والخصوصية ."
+namePpath = "https://trouko.com/subjects/technology/تعرف_على_مميزات_اندرويد_11/"
+infSuGuPageOn = '<p>  المواضيع / تكنلوجيا </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الاثنين  , 24 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
