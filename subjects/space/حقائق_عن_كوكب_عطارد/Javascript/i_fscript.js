db625fffaa8822040b0cf70cb30a2f10f37da6ce
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-05-25 16:06:49 +0300

    F

---
+++ subjects/space/حقائق_عن_كوكب_عطارد/Javascript/i_fscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "حقائق عن كوكب عطارد";
+adPhoto = "../../../imgs/mercury.jpg"
+adDiscription = "يعتبر عطارد أقرب كوكب إلى الشمس وبسبب قربه لا يمكن رؤيته بسهولة إلا خلال فترة الشفق. لكل مدارين للشمس ، يكمل عطارد ثلاث دورات حول محوره وحتى عام 1965 كان يُعتقد أن نفس الجانب من عطارد يواجه الشمس باستمرار.كما يمكن ملاحظة عطارد ثلاث عشرة مرة في القرن من الأرض يمر عبر وجه الشمس في حدث يسمى العبور ."
+namePpath = "https://trouko.com/subjects/space/حقائق_عن_كوكب_عطارد/"
+infSuGuPageOn = '<p>  المواضيع / الفضاء </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الثلاثاء  , 25 مايو 2021  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_n_232');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
---
+++ subjects/space/حقائق_عن_كوكب_عطارد/Javascript/script.js
@@ -0,0 +1,22 @@
+var addSourcesAndQuotesOne_n,addSourcesAndQuotesTwo_n ,addSourcesAndQuotesThree_n,addSourcesAndQuotesFour_n,addSourcesAndQuotesfive_n,addSourcesAndQuotesSix_n,
+    addSourcesAndQuotesSeven_n,addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,addSourcesAndQuotesOne_na,addSourcesAndQuotesTwo_na,addSourcesAndQuotesThree_na,addSourcesAndQuotesFour_na,addSourcesAndQuotesfive_na,addSourcesAndQuotesSix_na,addSourcesAndQuotesSeven_na,namePpath;
+    addSourcesAndQuotesOne_n = "ويكيبيديا الموسوعة الحرة - العربية";
+    addSourcesAndQuotesTwo_n = "";
+    addSourcesAndQuotesThree_n = "";
+    addSourcesAndQuotesFour_n = "";
+    addSourcesAndQuotesfive_n = "";
+    addSourcesAndQuotesSix_n = "";
+    addSourcesAndQuotesSeven_n
+    addSourcesAndQuotesOne_na = "https://ar.wikipedia.org/wiki/%D8%B9%D8%B7%D8%A7%D8%B1%D8%AF#%D8%A7%D9%84%D9%85%D8%AF%D8%A7%D8%B1_%D9%88%D8%A7%D9%84%D8%AF%D9%88%D8%B1%D8%A7%D9%86";
+    addSourcesAndQuotesTwo_na = "";
+    addSourcesAndQuotesThree_na = "";
+    addSourcesAndQuotesFour_na = "";
+    addSourcesAndQuotesfive_na = "";
+    addSourcesAndQuotesSix_na = "";
+    addSourcesAndQuotesSeven_na
+    addSugg_vrOne  = "https://trouko.com/subjects/space/حقائق_عن_كوكب_الزهرة/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/space/حقائق_عن_كوكب_نبتون/";
+    addSugg_vrThree  = "https://trouko.com/subjects/space/تضاريس_كوكب_المريخ/";
+    addSugg_vrFour  = "https://trouko.com/subjects/space/استعمار_المريخ/";
+    addSugg_vrFive = "https://trouko.com/subjects/space/أقمار_كوكب_المريخ/";
+    namePpath = "https://trouko.com/subjects/space/حقائق_عن_كوكب_عطارد";
