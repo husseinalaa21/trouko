91c949de3f85c3669cf0822a8445f0018d6f129f
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-06-02 21:02:08 +0300

    F

--- subjects/space/استعمار_المريخ/JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-var addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,namePpath;
-
-    addSugg_vrOne  = "https://www.trouko.co/questions/questions_space/ما_هي_الطاقة_المضلمة/";
-    addSugg_vrTwo  = "https://www.trouko.co/subjects/space/سبيس_اكس/";
-    addSugg_vrThree  = "https://www.trouko.co/subjects/space/مجرة_درب_التبانة/";
-    addSugg_vrFour  = "";
-    addSugg_vrFive = "";
-    namePpath = "https://trouko.com/subjects/space/استعمار_المريخ";
