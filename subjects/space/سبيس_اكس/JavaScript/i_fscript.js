2665ca310aa1f6cb15b7fef1e4b23e96ad0ada4a
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-06-02 21:09:13 +0300

    F

--- subjects/space/سبيس_اكس/JavaScript/i_fscript.js
+++
@@ -1,5 +0,0 @@
-var letHu  = document.getElementById("whereHu").innerText
-var addTitleIpage = document.getElementById(letHu)
-
-addTitleIpage.innerHTML = '<div class="img_p_sugg_p"> <a href="https://trouko.com/subjects/space/سبيس_اكس"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/SpaceX_Logo_Black.png/1920px-SpaceX_Logo_Black.png" alt="سبيس اكس"  class="sub_s_i" style="width:100%" height="auto"> </a> </div> <div class="sugg_p_t_bo_23"> <a class="s_sub_t" href="https://trouko.com/subjects/space/سبيس_اكس">سبيس اكس</a> <div class="infoPageSuQu"><p> المواضيع / الفضاء</p><i class="material-icons" style="font-size:16px; color: #393e46;">folder</i></div><div class="p_t_inf" > <p>و تعرف بتقنيات استكشاف الفضاء ويطلق عليها اسم سبيس إكس وهي شركة خاصة أمريكية تختص بصناعات هندسة الطي... </p><a class="link_t_read_p" href="https://trouko.com/subjects/space/سبيس_اكس"> قراءة المزيد </a></div><div class="infoPageSuQuTw"><p> الثلاثاء, 27 اكتوبر 2020  </p><i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i></div></div>'
-reqHussein()
--- subjects/space/ناسا/JavaScript/i_fscript.js
+++
@@ -1,5 +0,0 @@
-var letHu  = document.getElementById("whereHu").innerText
-var addTitleIpage = document.getElementById(letHu)
-
-addTitleIpage.innerHTML = '<div class="img_p_sugg_p"> <a href="https://trouko.com/subjects/space/ناسا"> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/800px-NASA_logo.svg.png" alt="ناسا"  class="sub_s_i" style="width:100%" height="auto"> </a> </div> <div class="sugg_p_t_bo_23"> <a class="s_sub_t" href="https://trouko.com/subjects/space/ناسا">ناسا</a> <div class="infoPageSuQu"><p> المواضيع / الفضاء</p><i class="material-icons" style="font-size:16px; color: #393e46;">folder</i></div><div class="p_t_inf" > <p>افتتحت وكالة ناسا للعمل في 1 أكتوبر 1958 . وتم إنشاء الوكالة للإشراف على استكشاف الفضاء الأمريكي وأب... </p><a class="link_t_read_p" href="https://trouko.com/subjects/space/ناسا"> قراءة المزيد </a></div><div class="infoPageSuQuTw"><p> الثلاثاء, 27 اكتوبر 2020  </p><i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i></div></div>'
-reqHussein()
