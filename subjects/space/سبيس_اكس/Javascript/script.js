2e8902da36224391dbf2b7aed66807316a9a00d4
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-06-02 21:10:19 +0300

    F

---
+++ subjects/space/سبيس_اكس/Javascript/script.js
@@ -0,0 +1,8 @@
+var addSugg_vrOne,addSugg_vrTwo,addSugg_vrThree,addSugg_vrFour,addSugg_vrFive,namePpath;
+
+    addSugg_vrOne  = "https://trouko.com/subjects/space/ناسا/";
+    addSugg_vrTwo  = "https://trouko.com/subjects/space/استعمار_المريخ/";
+    addSugg_vrThree  = "https://trouko.com/subjects/space/مجرة_درب_التبانة/";
+    addSugg_vrFour  = "";
+    addSugg_vrFive = "";
+    namePpath = "https://trouko.com/subjects/space/سبيس_اكس";
---
+++ subjects/space/ناسا/Javascript/i_tnfscript.js
@@ -0,0 +1,16 @@
+var titleIn , addTitleIpage ,nameP , namePpath, discriptionP , photoP,infSuGuPageOn , infSuGuPageTw;
+titleIn = "ناسا";
+adPhoto = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e5/NASA_logo.svg/800px-NASA_logo.svg.png"
+adDiscription = "افتتحت وكالة ناسا للعمل في 1 أكتوبر 1958 . وتم إنشاء الوكالة للإشراف على استكشاف الفضاء الأمريكي وأبحاث الطيران.لتقف على الإدارة الوطنية للملاحة الجوية والفضاء. و هي وكالة حكومية أمريكية مسؤولة عن العلوم والتكنولوجيا المتعلقة بالهواء والفضاء. بدأ عصر الفضاء عام 1957 بإطلاق القمر الصناعي السوفيتي سبوتنيك."
+namePpath = "https://trouko.com/subjects/space/ناسا/"
+infSuGuPageOn = '<p>  المواضيع / الفضاء </p>'+'<i class="material-icons" style="font-size:16px; color: #393e46;">folder</i>'
+infSuGuPageTw = '<p> الثلاثاء, 27 اكتوبر 2020  </p>'+'<i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i>'
+nameP = namePpath.replace('https://trouko.com/',"")
+addTitleIpage = document.getElementById(nameP+'_pag_s_262');
+addt(titleIn)
+function addt(m){
+var textDis = adDiscription.slice(0, 80);
+var ap = document.createElement('div')
+ap.innerHTML ='<div class="img_p_sugg_p"> <a href="'+namePpath+'">'+'<img src="'+adPhoto +'" alt="'+m+'"  class="sub_s_i" style="width:100%"> </a> </div>' + "<div class='sugg_p_t_bo_23'> <a class='s_sub_t' href='"+namePpath+"'>"+m+"</a>"+'<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">'+'<div class="infoPageSuQu">'+infSuGuPageOn+'</div>'+"<div class='p_t_inf'> <p>"+textDis +"... </p>"+ '<a class="link_t_read_p" href="'+namePpath+'"> قراءة المزيد </a>'+"</div>"+'<div class="infoPageSuQuTw">'+infSuGuPageTw+'</div>'+ "</div>"
+addTitleIpage.append(ap)
+}
