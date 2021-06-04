ca23359e2185626bfa1ae15f08d42e81f23fb67d
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-06-04 15:28:28 +0300

    F

---
+++ subjects/geography/أنواع_الحدائق/Javascript/i_fscript.js
@@ -0,0 +1,5 @@
+var letHu  = document.getElementById("whereHu").innerText
+var addTitleIpage = document.getElementById(letHu)
+
+addTitleIpage.innerHTML = '<div class="img_p_sugg_p"> <a href="https://trouko.com/subjects/geography/أنواع_الحدائق"> <img src="../../../imgs/gardens.webp" alt="أنواع الحدائق"  class="sub_s_i" style="width:100%" height="auto"> </a> </div> <div class="sugg_p_t_bo_23"> <a class="s_sub_t" href="https://trouko.com/subjects/geography/أنواع_الحدائق">أنواع الحدائق</a> <div class="infoPageSuQu"><p> المواضيع / الجغرافية</p><i class="material-icons" style="font-size:16px; color: #393e46;">folder</i></div><div class="p_t_inf" > <p>الحديقة هى مساحة من التربة التي تغطيها الحشائش و بعض أنواع النباتات, عادة ما تكون الحدائق خارج جدران... </p><a class="link_t_read_p" href="https://trouko.com/subjects/geography/أنواع_الحدائق"> قراءة المزيد </a></div><div class="infoPageSuQuTw"><p> الجمعه, 4 يونيو 2021  </p><i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i></div></div>'
+reqHussein()
