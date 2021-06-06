4eee516075a002394dd3197b159a3a100dcb269a
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-06-06 19:38:07 +0300

    F

---
+++ subjects/technology/مكونات_الحاسوب/Javascript/i_fscript.js
@@ -0,0 +1,5 @@
+var letHu  = document.getElementById("whereHu").innerText
+var addTitleIpage = document.getElementById(letHu)
+
+addTitleIpage.innerHTML = '<div class="img_p_sugg_p"> <a href="https://trouko.com/subjects/technology/مكونات_الحاسوب"> <img src="../../../imgs/computerWebWord.webp" alt="مكونات الحاسوب"  class="sub_s_i" style="width:100%" height="auto"> </a> </div> <div class="sugg_p_t_bo_23"> <a class="s_sub_t" href="https://trouko.com/subjects/technology/مكونات_الحاسوب">مكونات الحاسوب</a> <div class="infoPageSuQu"><p> المواضيع / تكنلوجيا</p><i class="material-icons" style="font-size:16px; color: #393e46;">folder</i></div><div class="p_t_inf" > <p>الحاسوب هو جهاز يستطيع تخزين المعلومات و البيانات ومعالجتها. كما و تعتمد معظم أجهزة الحاسوب على نظام... </p><a class="link_t_read_p" href="https://trouko.com/subjects/technology/مكونات_الحاسوب"> قراءة المزيد </a></div><div class="infoPageSuQuTw"><p> الاحد, 6 يونيو 2021  </p><i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i></div></div>'
+reqHussein()
