4d06263a893a2c86b66e758696340d4e45009b17
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-06-14 20:43:43 +0300

    F

---
+++ questions/questions_else/كيفية_أخذ_القياسات_لتفصيل_بدلة/Javascript/i_fscript.js
@@ -0,0 +1,5 @@
+var letHu  = document.getElementById("whereHu").innerText
+var addTitleIpage = document.getElementById(letHu)
+
+addTitleIpage.innerHTML = '<div class="img_p_sugg_p"> <a href="https://trouko.com/questions/questions_else/كيفية_أخذ_القياسات_لتفصيل_بدلة"> <img src="../../../imgs/alexander-andrews-jNKv4QohAk0-unsplash.webp" alt="كيفية أخذ القياسات لتفصيل بدلة"  class="sub_s_i" style="width:100%" height="auto"> </a> </div> <div class="sugg_p_t_bo_23"> <a class="s_sub_t" href="https://trouko.com/questions/questions_else/كيفية_أخذ_القياسات_لتفصيل_بدلة">كيفية أخذ القياسات لتفصيل بدلة</a> <div class="infoPageSuQu"><p>الاسئلة / متنوعة</p><i class="material-icons" style="font-size:16px; color: #393e46;">folder</i></div><div class="p_t_inf" > <p>إذا أردت أن تظهر بمظهر أنيق وجميل فلا شيء يبدو أفضل من بدلة مصممة بشكل جيد،كما أن البدلة المصممة  تم... </p><a class="link_t_read_p" href="https://trouko.com/questions/questions_else/كيفية_أخذ_القياسات_لتفصيل_بدلة"> قراءة المزيد </a></div><div class="infoPageSuQuTw"><p> الاثنين, 14 يونيو 2021  </p><i class="material-icons" style="font-size:15px; color: #393e46; margin-right: 5px;">date_range</i></div></div>'
+reqHussein()
