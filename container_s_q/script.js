711a182c9390b582c09ceaad842d5419cd6bf32d
husseinalaa21 <huseinalzubaidi@hotmail.com>
2021-06-19 18:18:27 +0300

    F

--- container_s_q/script.js
+++ container_s_q/script.js
@@ -1,20 +1,3 @@
-var someEncodedString = encodeURI(window.location.href)
-var zare = document.getElementsByClassName("time_write_this_page_o")
-var a = document.createElement("a")
-
-window.onload = function () {
-    document.getElementById("endGPage").innerHTML = `<img src="../../../icons/check-circle-regular.svg" style="width: 9px;"> &nbsp;  تروكو هو موقع لنشر المقالات المختلفة باللغة العربية , الهدف الرئيسي من أنشائه هو لرفع نسبة
-    المقالات المطروحة باللغة العربية.`
-    a.className = "sheThPage twitShe"
-    a.href = 'http://twitter.com/share?text=' + document.getElementById("titleTs").innerText + '&url=' + someEncodedString
-    a.innerHTML = '<img src="../../../icons/twitter-m.svg" width="13px" height="13px"/>'
-    if(zare.length === 1){
-        zare[0].append(a)
-    } else {
-        zare[1].append(a)
-    }
-}
-
 var xReq = new XMLHttpRequest();
 xReq.open('GET', '../data.js');
 
